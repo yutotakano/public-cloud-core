02e8110e9c8ccba215d99c797729f823785d55c5
Andrew Ferguson <andrewferguson500@gmail.com>
2021-03-28 15:40:10 +0100

    Modified InitialContextSequpRequest to cope with new UE_NAS_SQN
    The way that the DB handles UE_NAS_SQN has changed - it no longer
    subtracts 1 from the value, so the InitialContextSetupRequest
    must do this itself.
    
    Note that this approach (getting UE_NAS_SQN from the DB then
    extracting 1 from it) is not a suitable approach - it will cause
    bugs later down the road, and this approach has been marked as
    requiring a rewrite.

--- corekube_worker/source/initialcontextsetuprequest.c
+++ corekube_worker/source/initialcontextsetuprequest.c
@@ -67,12 +67,12 @@ status_t attach_accept_fetch_state(S1AP_MME_UE_S1AP_ID_t *mme_ue_id, c_uint8_t *
     //          nas_security_encode():
     //              epc_nas_sequence_number, enc_key, int_key
     //     s1ap_build_initial_context_setup_request():
-    //          mme_ue_s1ap_id, enb_ue_s1ap_id
+    //          mme_ue_s1ap_id, enb_ue_s1ap_id, kasme1, kasme2, ue_nas_sequence_number
 
-    const int NUM_PULL_ITEMS = 7;
+    const int NUM_PULL_ITEMS = 8;
     n = push_items(buffer, MME_UE_S1AP_ID, (uint8_t *)raw_mme_ue_id.buf, 0);
     n = pull_items(buffer, n, NUM_PULL_ITEMS,
-        MME_UE_S1AP_ID, ENB_UE_S1AP_ID, INT_KEY, ENC_KEY, EPC_NAS_SEQUENCE_NUMBER, KASME_1, KASME_2);
+        MME_UE_S1AP_ID, ENB_UE_S1AP_ID, INT_KEY, ENC_KEY, EPC_NAS_SEQUENCE_NUMBER, UE_NAS_SEQUENCE_NUMBER, KASME_1, KASME_2);
     send_request(sock, buffer, n);
     n = recv_response(sock, buffer, 1024);
     db_disconnect(sock);
@@ -271,7 +271,12 @@ status_t s1ap_build_initial_context_setup_request(
     memcpy(kasme+SHA256_DIGEST_SIZE/2, db_pulls->kasme2, SHA256_DIGEST_SIZE/2);
 
     // the ul_count is stored in the DB as 6-bytes, we only care about 4-bytes
-    c_uint32_t nas_ul_count = array_to_int(db_pulls->ue_nas_sequence_number+2);
+    // TODO: the "-1" is a very temporary fix and MUST BE CORRECTED - although it
+    // will work fine for generating the Kenb, from now on the UE_NAS_SQN will be
+    // out by one. Instead, the proper approach would be to get all the DB
+    // prerequisites at the start of handling the Security Mode Command, and use
+    // the UE_NAS_SQN from there.
+    c_uint32_t nas_ul_count = array_to_int(db_pulls->ue_nas_sequence_number+2) - 1;
 
     // calculate the Kenb from Kasme
     kdf_enb(kasme, nas_ul_count, kenb);
