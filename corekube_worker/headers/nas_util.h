5a1c6eb9b4a9de3b634e3d006bd5104b97c73cae
Andrew Ferguson <andrewferguson500@gmail.com>
2021-03-28 15:45:13 +0100

    Make decode_nas_emm() auto-detect plain and protected messages

--- corekube_worker/headers/nas_util.h
+++ corekube_worker/headers/nas_util.h
@@ -5,6 +5,6 @@
 #include "s1ap/s1ap_message.h"
 #include <libck.h>
 
-status_t decode_nas_emm(S1AP_NAS_PDU_t *NAS_PDU, corekube_db_pulls_t *db_pulls, nas_message_t *nas_message);
+status_t decode_nas_emm(S1AP_NAS_PDU_t *NAS_PDU, S1AP_MME_UE_S1AP_ID_t *mme_ue_id, nas_message_t *nas_message);
 
 #endif /* __S1AP_HANDLER_NAS_UTIL_H__ */
