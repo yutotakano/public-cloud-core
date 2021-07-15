392872822cc45f138d3f8134e75ca6269346ac0a
Andrew Ferguson <andrewferguson500@gmail.com>
2021-07-15 20:18:09 +0100

    Removed redundant function from header file

--- headers/nas_registration_request.h
+++ headers/nas_registration_request.h
@@ -13,6 +13,4 @@ extern int __corekube_log_domain;
 
 int nas_handle_registration_request(ogs_nas_5gs_registration_request_t *message, nas_ngap_params_t *params, message_handler_response_t * response);
 
-int get_sample_auth_response(ogs_nas_5gs_message_t *message);
-
 #endif /* __COREKUBE_NAS_REGISTRATION_REQUEST_H__ */
