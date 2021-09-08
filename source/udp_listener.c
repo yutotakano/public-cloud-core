38992c24b31c1bca523f39f16fc1377ba3e6ef3b
Andrew Ferguson <andrewferguson500@gmail.com>
2021-09-08 14:26:22 +0100

    Make worker crash if unable to connect to the DB

--- source/udp_listener.c
+++ source/udp_listener.c
@@ -217,6 +217,7 @@ int main(int argc, char const *argv[])
 	//db_ip_address = (char*) core_calloc(strlen((char *)argv[2]), sizeof(char));
 	//memcpy(db_ip_address, (char *)argv[2], strlen((char *)argv[2]));
 	db_sock = db_connect((char *)argv[2], 0);
+	ogs_assert(db_sock != -1);
 
 	start_listener((char *)argv[1], use_threads);
 
