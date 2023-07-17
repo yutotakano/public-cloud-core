08982a66998e8568387c250a5e0a153b7e14d7ed
Andrew Ferguson <andrewferguson500@gmail.com>
2023-07-17 11:57:54 -0600

    Bugfix: Ensure every DB push has a pull to avoid errors in the DB protocol
    There is an error in the DB protocol implementation whereby doing
    a push without a corresponding pull can cause a later DB access to
    fail when it receives null bytes.
    
    This error may(?) be caused by the DB sending a reply to the push-only
    access, even though no reply is expected. This reply, consisting
    of only null bytes, is then read by the next DB access instead of
    the values it was trying to access. This causes the error.
    
    Rather than reimplementing the DB protocol, ensuring that every DB
    push has a correspinding pull prevents this error from occuring, at
    the slight cost of increased communication inefficiencies (transferring
    an extra value from the DB that will not be used).

--- source/ngap_pdu_session_resource_setup_response_transfer.c
+++ source/ngap_pdu_session_resource_setup_response_transfer.c
@@ -75,8 +75,10 @@ int nas_store_ngap_pdu_session_resource_setup_response_transfer_fetch_prerequisi
     ogs_asn_uint32_to_OCTET_STRING( (uint32_t) params->amf_ue_ngap_id, &amf_ue_ngap_id_buf);
 
     // store the UE_TEID and ENB_IP into the database
-    int db = db_access(NULL, MME_UE_S1AP_ID, amf_ue_ngap_id_buf.buf, 2, 0, UE_TEID, params->ue_teid, ENB_IP, params->enb_ip);
+    corekube_db_pulls_t dbp;
+    int db = db_access(&dbp, MME_UE_S1AP_ID, amf_ue_ngap_id_buf.buf, 2, 1, UE_TEID, params->ue_teid, ENB_IP, params->enb_ip, PDN_IP);
     ogs_assert(db == OGS_OK);
+    ogs_free(dbp.head);
 
     // free the buffer used for the DB access
     ogs_free(amf_ue_ngap_id_buf.buf);
