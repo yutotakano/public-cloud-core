3134f4a8abeb1f307de62d388219adf43454c0eb
Yuto Takano <moa17stock@gmail.com>
2024-02-13 08:43:52 +0000

    Separate deployment info gathering into its own function for reuse

--- include/info.h
+++ include/info.h
@@ -5,6 +5,13 @@
 #include "executor.h"
 #include "quill/Quill.h"
 
+struct deployment_info_s
+{
+  std::string corekube_dns_name;
+  std::string nervion_dns_name;
+  std::string frontend_ip;
+};
+
 class InfoApp
 {
 public:
@@ -13,6 +20,8 @@ public:
   std::unique_ptr<argparse::ArgumentParser> info_arg_parser();
 
   void info_command_handler(argparse::ArgumentParser &parser);
+
+  std::optional<deployment_info_s> get_info();
 
 private:
   quill::Logger *logger;
--- source/info.cpp
+++ source/info.cpp
@@ -16,17 +16,35 @@ std::unique_ptr<argparse::ArgumentParser> InfoApp::info_arg_parser()
 
 void InfoApp::info_command_handler(argparse::ArgumentParser &parser)
 {
+  LOG_TRACE_L3(logger, "Getting information about the current deployment.");
+
+  auto info = get_info();
+  if (!info.has_value())
+  {
+    LOG_ERROR(logger, "Could not get info: no active deployment found.");
+    return;
+  }
+
+  LOG_INFO(logger, "CoreKube deployment found.");
+  LOG_INFO(logger, "IP (within VPC) of CK frontend: {}", info->frontend_ip);
+  LOG_INFO(logger, "Grafana: http://{}:3000", info->corekube_dns_name);
+  LOG_INFO(logger, "Nervion: http://{}:8080", info->nervion_dns_name);
+}
+
+std::optional<deployment_info_s> InfoApp::get_info()
+{
   LOG_DEBUG(logger, "Looking for active deployment.");
 
   // Check if there is an active deployment
+  LOG_TRACE_L3(logger, "Checking existing contexts");
   auto contexts_str =
     executor.run({"kubectl", "config", "get-contexts", "--output=name"}, false)
       .future.get();
   auto contexts = Utils::split(contexts_str, '\n');
   if (contexts.size() == 1 && contexts[0].empty())
   {
-    LOG_ERROR(logger, "No active deployment found.");
-    return;
+    LOG_TRACE_L3(logger, "No active deployment found.");
+    return std::nullopt;
   }
 
   // Double-check that there is a CoreKube and Nervion deployment
@@ -36,24 +54,28 @@ void InfoApp::info_command_handler(argparse::ArgumentParser &parser)
   {
     if (context.find("corekube") != std::string::npos)
     {
+      LOG_TRACE_L3(logger, "CoreKube context found: {}", context);
       corekube_name = context;
     }
     if (context.find("nervion") != std::string::npos)
     {
+      LOG_TRACE_L3(logger, "Nervion context found: {}", context);
       nervion_name = context;
     }
   }
   if (corekube_name.empty() || nervion_name.empty())
   {
-    LOG_ERROR(logger, "CoreKube or Nervion deployment not found.");
-    return;
+    LOG_TRACE_L3(logger, "No active deployment found.");
+    return std::nullopt;
   }
 
   // Switch to the CoreKube context to get information about frontend
+  LOG_TRACE_L3(logger, "Switching to CoreKube context");
   executor.run({"kubectl", "config", "use-context", corekube_name}, false)
     .future.get();
 
   // Get the VPC-internal IP of the frontend node
+  LOG_TRACE_L3(logger, "Getting frontend IP");
   auto frontend_ip =
     executor
       .run(
@@ -70,6 +92,7 @@ void InfoApp::info_command_handler(argparse::ArgumentParser &parser)
       .future.get();
 
   // Get the public DNS of the Grafana service.
+  LOG_TRACE_L3(logger, "Getting Grafana public DNS");
   auto ck_grafana_public_dns =
     executor
       .run(
@@ -83,9 +106,11 @@ void InfoApp::info_command_handler(argparse::ArgumentParser &parser)
       .future.get();
 
   // Switch to the Nervion context to get information about Nervion controller
+  LOG_TRACE_L3(logger, "Switching to Nervion context");
   executor.run({"kubectl", "config", "use-context", nervion_name}, false)
     .future.get();
 
+  // Get the public DNS of the Nervion controller service.
   auto nervion_controller_public_dns =
     executor
       .run(
@@ -99,9 +124,9 @@ void InfoApp::info_command_handler(argparse::ArgumentParser &parser)
       )
       .future.get();
 
-  LOG_INFO(logger, "CoreKube deployment found.");
-  LOG_INFO(logger, "IP (within VPC) of CK frontend node: {}", frontend_ip);
-  LOG_INFO(logger, "Grafana: http://{}:3000", ck_grafana_public_dns);
-  LOG_INFO(logger, "Nervion: http://{}:8080", nervion_controller_public_dns);
-  LOG_INFO(logger, "Current cluster: nervion-aws-cluster");
+  return deployment_info_s{
+    .corekube_dns_name = ck_grafana_public_dns,
+    .nervion_dns_name = nervion_controller_public_dns,
+    .frontend_ip = frontend_ip,
+  };
 }
