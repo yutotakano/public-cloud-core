a220d8ee5deee10d029efad7f890d4733b25b9a9
Yuto Takano <moa17stock@gmail.com>
2024-03-05 13:10:29 +0000

    Use 192.168 range for VPC

--- terraform/base/main.tf
+++ terraform/base/main.tf
@@ -48,11 +48,11 @@ module "vpc" {
 
   name = "corekube-vpc"
 
-  cidr = "10.0.0.0/16"
+  cidr = "192.168.0.0/16"
   azs  = slice(data.aws_availability_zones.available.names, 0, 3)
 
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  private_subnets = ["192.168.1.0/24", "192.168.2.0/24", "192.168.3.0/24"]
+  public_subnets  = ["192.168.4.0/24", "192.168.5.0/24", "192.168.6.0/24"]
 
   enable_nat_gateway = true
   single_nat_gateway = true
